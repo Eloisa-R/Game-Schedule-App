b22967d705d4fa5549c98e5e1efe5f25b18b5f65
Eloisa-R <rios.rico.eloisa@gmail.com>
2018-04-05 20:07:05 +0200

    Added calendar for testing

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 class Calendar extends Component {
+    
+    displayCalendar() {
+        return <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;hl=en&amp;bgcolor=%23FFFFFF&amp;src=p3bpcomi895snhkrt9dog1g0so%40group.calendar.google.com&amp;color=%230F4B38&amp;ctz=Europe%2FMadrid" width="80%" height="65%" frameBorder="0" scrolling="no"></iframe>
+    }
   render() {
     return (
       <div className="calendar-body">
-        <p>The calendar will go here</p>
+        {this.displayCalendar()}
       </div>
     );
   }
