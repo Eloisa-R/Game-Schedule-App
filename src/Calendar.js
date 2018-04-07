5eeec36675e963a3594fd34672869030bc69af76
Eloisa-R <rios.rico.eloisa@gmail.com>
2018-04-07 15:49:26 +0200

    FIxed calendar display

--- src/Calendar.js
+++ src/Calendar.js
@@ -8,12 +8,16 @@ import {
 import MatchesMonth from "./MatchesMonth";
 
 class Calendar extends Component {
-
     
+      hideDiv() {
+            var alertDiv = document.getElementById("click-on-month");
+            alertDiv.style.display = "none";
+      }
+
     displayMenu() {
         let month_array = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
         var month_list = month_array.map((item, index) =>
-                                        <NavLink key={index} to={`${this.props.match.url}/matchesmonth/${index}-${item}`} className="month_item"><li>{item}</li></NavLink>)
+                                        <NavLink key={index} to={`${this.props.match.url}/matchesmonth/${index}-${item}`} onClick={this.hideDiv} className="month_item"><li>{item}</li></NavLink>)
         return <ul className="month_menu">{month_list}</ul>
     }
 
@@ -23,6 +27,7 @@ class Calendar extends Component {
       <div className="calendar-body">
        {this.displayMenu()}
        <div className="eventsBody">
+         <div id="click-on-month">Click on a month to load the schedule</div>
          <Route path={`${this.props.match.path}/matchesmonth/:id`} component={MatchesMonth}/>
         </div>   
       </div>
--- src/MatchesMonth.js
+++ src/MatchesMonth.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
 
 class MatchesMonth extends Component {
-    displayEvents() {
-    var index_clicked = this.props.match.params.id.split("-")[0];
-    var item_clicked = this.props.match.params.id.split("-")[1];
+    constructor() {
+        super();
+        this.state = {
+            events: "",
+            matches_list: "",
+            matches_of_the_month: []
+            
+        }
+    }
+    
+    componentDidUpdate() {
+    
+    let index_clicked = this.props.match.params.id.split("-")[0];
+    let item_clicked = this.props.match.params.id.split("-")[1];
     fetch("https://api.myjson.com/bins/1eo3yz").then((response)=>{
         return response.json().then((json) =>{
-            var matches_of_the_month = json.months[index_clicked][item_clicked].matches
-            var matches_list = matches_of_the_month.map((element,index)=>
-                                    <div key={index}><h3>Teams: {element.teams}</h3><p>Date: {element.date}</p><p>Location: {element.location}</p></div>)
-            return <div>{matches_list}</div>
-            
+            this.matches_of_the_month = json.months[index_clicked][item_clicked].matches
+            this.matches_list = this.matches_of_the_month.map((element,index)=>
+                                    <div key={index} className="schedule-team-container"><h3 className="schedule-team-header">Teams: {element.teams}</h3><div className="schedule-team-body"><p>Date: {element.date}</p><p>Location: {element.location}</p></div></div>)
+            let final_result = <div>{this.matches_list}</div>
+            this.setState({events:final_result})
         })
     })
-      return <div>Hey, Dave</div>
+      
     }
     
     render() {
     return (
         <div>
-        <div><p>Hi there {this.props.match.params.id}</p></div>
-        <div>{this.displayEvents()}</div>
+        <div><p>{this.props.match.params.id.split("-")[1]}</p></div>
+        <div className="schedule-container">{this.state.events}</div>
         </div>
     )
     }
