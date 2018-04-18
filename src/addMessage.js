3163e53b8c52958ce815bb31aaf2b523848af092
Eloisa-R <rios.rico.eloisa@gmail.com>
2018-04-18 12:33:51 +0200

    Added image uploading functionality

--- src/addMessage.js
+++ src/addMessage.js
@@ -9,10 +9,11 @@ class addMessage extends Component {
     this.state = {
       title: "",
       messages: "",
-      chat_id: ""
+      chat_id: "",
     }
       this.showMessages = this.showMessages.bind(this);
       this.postMessage = this.postMessage.bind(this);
+      this.handleFiles = this.handleFiles.bind(this);
     }
   showMessages() {
       firebase.database().ref('/messages/'+ this.props.match.params.type).orderByChild('timestamp').on('value', (chat) =>{
@@ -22,9 +23,17 @@ class addMessage extends Component {
          })
          this.setState({title: this.props.match.params.id, chat_id: this.props.match.params.type, messages: temp_messages.map((element, index) => {
               if (element[0] == auth.currentUser.displayName) {
-                return <div className="message user" key={index}><div className="author"><div>{element[0]}</div><div>Sent: {moment.unix(element[2]/1000).format("DD/MM/YYYY")}</div></div><p className="text">{element[1]}</p></div>  
+                if (element[1].includes("firebasestorage")){
+                  return <div className="message user" key={index}><div className="author"><div>{element[0]}</div><div>Sent: {moment.unix(element[2]/1000).format("DD/MM/YYYY")}</div></div><p className="imageSent"><img alt="sentFile" src={element[1]}/></p></div>
+                } else {
+                  return <div className="message user" key={index}><div className="author"><div>{element[0]}</div><div>Sent: {moment.unix(element[2]/1000).format("DD/MM/YYYY")}</div></div><p className="text">{element[1]}</p></div>
+                }
               } else {
-              return <div className="message" key={index}><div className="author"><div>{element[0]}</div> <div>Sent: {moment.unix(element[2]/1000).format("DD/MM/YYYY")}</div></div><p className="text">{element[1]}</p></div>
+                if (element[1].includes("firebasestorage")){
+                  return <div className="message" key={index}><div className="author"><div>{element[0]}</div><div>Sent: {moment.unix(element[2]/1000).format("DD/MM/YYYY")}</div></div><p className="imageSent"><img alt="sentFile" src={element[1]}/></p></div>
+                } else {
+                  return <div className="message" key={index}><div className="author"><div>{element[0]}</div><div>Sent: {moment.unix(element[2]/1000).format("DD/MM/YYYY")}</div></div><p className="text">{element[1]}</p></div>
+                }
             }
             })})
       })
@@ -44,6 +53,30 @@ class addMessage extends Component {
             console.log("please write a message")
         }
     }
+
+  handleFiles(e){
+    let uploadedFile = e.target.files;
+    if (uploadedFile.length > 0) {
+      let storageRef = firebase.storage().ref();
+      let fileRef = storageRef.child(uploadedFile[0].name);
+      let fileFolderRef = storageRef.child(this.state.title + '/' + uploadedFile[0].name);
+      let uploadTask = fileFolderRef.put(uploadedFile[0])
+      uploadTask.on('state_changed',(snapshot) =>{
+        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+      }, (error) => {
+        console.log("Oopsie")
+      }, () => {
+        let downloadURL= uploadTask.snapshot.downloadURL;
+        let key = firebase.database().ref('/').push().key
+        let author = auth.currentUser.displayName
+        let model = messageModel(key,author,downloadURL,firebase.database.ServerValue.TIMESTAMP)
+        return firebase.database().ref('/messages/'+ this.state.chat_id + "/" + key).set(model)
+        this.showMessages()
+      });
+    }
+    
+  }
     
                                                                                     
   componentDidMount() {
@@ -60,7 +93,7 @@ class addMessage extends Component {
         :
         <img alt="loader" id="loaderGif" src={require("./images/load-dribbble.gif")}/>
         }
-        <div className="chat-submit"><form><input id="chatMessage" type="text"/><input onClick={(e) => this.postMessage(e)} type="submit" value="Send"/></form></div>
+        <div className="chat-submit"><form id="addMform"><input id="chatMessage" type="text"/><input onClick={(e) => this.postMessage(e)} type="submit" value="Send"/></form><label htmlFor="inputFiles" className="btn"><img alt="attach" id="attachClip" src={require("./images/Attach-512.png")}/></label><input type="file" id="inputFiles"  onChange={(e) => this.handleFiles(e)}/></div>
       </div>
     );
   }
